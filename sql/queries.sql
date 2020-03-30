f6c19081b464100bec5faf8d7e9294e895cb0141
Wen Li <i1s2b@ugrad.cs.ubc.ca>
2020-03-29 19:26:06 -0700

    corrected some queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -22,31 +22,44 @@ Go --remove Go in mysql
 --Displays all players with a z-score higher than x
 --Selection
 --TODO// replace 1 with user input x
-Select P.name AS Player, P.number AS Num, P.teamID AS Team, P.zscore AS 'Z-Score'
-From playerplaysfor P
-Where 'Z-Score' >= 1;
+Select P.name AS Player, P.number AS Num, T.name AS Team, P.zscore AS 'Z-Score'
+From playerplaysfor P inner join teamplaysin T
+On P.teamID=T.id
+Where P.zscore >= 1;
 
 --Displays all players who placed x in year y
 --Division
---TODO// replace 1 with user input x, 2016 with user input y
+--TODO// replace 'First' with user input x, 2012 with user input y
+--TODO// doesnt work
 Select P.name As Player
 From playerplaysfor P
 Where not exists
-(Select * 
+(Select S.teamID
 From seasonof S 
 Where not exists
-(Select T.name
+(Select T.id
 From teamplaysin T
-Where T.name = S.teamID AND T.name = P.teamID AND S.year = 2016 AND
-S.standing = 1));
+Where T.id = P.teamID AND T.id = S.teamID AND S.year = 2012 AND
+S.standing = 'First'));
+
+--TODO// except version, select is invalid error
+-- Select P.name As Player
+-- From playerplaysfor P
+-- Where not exists (
+-- (Select S.teamID from seasonof S)
+-- Except
+-- (Select T.id from teamplaysin T
+-- where T.id=P.teamID and S.year = 2012 and
+-- S.standing = 'First'));
 
 --Displays teams with number of sponsors and total amount of $ funded by all 
 --sponsors combined
 --Aggregation
-Select F.teamID As Team, count(Distinct F.sponsorID) As '# of Sponsors',
+Select T.name As Team, count(Distinct F.sponsorID) As '# of Sponsors',
 sum(Distinct F.amount) As 'Total Sponsorship$'
-From funds F
+From funds F, teamplaysin T
+where F.teamID=T.id
 Group by Team
-Order by Team;
+Order by sum(Distinct F.amount) DESC;
 
 --TODO// Projection
